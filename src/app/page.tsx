72924635b71bfc79afe6e5cb955866775c6df4ec
Mach Wonder <426821+noahwatsondev@users.noreply.github.com>
2025-05-13 23:40:41 -0500

    Personalize homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Noah Watson Dev{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
+              Portfolio
             </code>
             .
           </li>
